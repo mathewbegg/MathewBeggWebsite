bf0e589f77de4e6ef4a14f48948ae6bac072db67
Mathew <mathew.r.begg@gmail.com>
2020-12-21 22:32:52 -0700

    added ripple effect to project view cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MainLayoutComponent } from './main-layout/main-layout.component'
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 import {
   MatToolbarModule,
   MatMenuModule,
@@ -12,13 +12,14 @@ import {
   MatTabsModule,
   MatTooltipModule,
   MatDialogModule,
-} from '@angular/material'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { ProjectViewComponent } from './project-view/project-view.component'
-import { AppRoutingModule } from './app-routing.module'
-import { AboutMeViewComponent } from './about-me-view/about-me-view.component'
-import { ResumeViewComponent } from './resume-view/resume-view.component'
-import { ProjectDialogComponent } from './project-dialog/project-dialog.component'
+  MatRippleModule,
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProjectViewComponent } from './project-view/project-view.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutMeViewComponent } from './about-me-view/about-me-view.component';
+import { ResumeViewComponent } from './resume-view/resume-view.component';
+import { ProjectDialogComponent } from './project-dialog/project-dialog.component';
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { ProjectDialogComponent } from './project-dialog/project-dialog.componen
     MatTabsModule,
     MatTooltipModule,
     MatDialogModule,
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
