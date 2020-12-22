c268bff5a6a0b8ba1b0d033ee3c40a4bbaa1c654
Mathew <mathew.r.begg@gmail.com>
2020-12-22 00:04:56 -0700

    Home screen redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { ProjectViewComponent } from "./project-view/project-view.component";
-import { AboutMeViewComponent } from "./about-me-view/about-me-view.component";
-import { ResumeViewComponent } from "./resume-view/resume-view.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectViewComponent } from './project-view/project-view.component';
+import { AboutMeViewComponent } from './about-me-view/about-me-view.component';
+import { ResumeViewComponent } from './resume-view/resume-view.component';
 
 const routes: Routes = [
-  { path: "aboutMe", component: AboutMeViewComponent },
-  { path: "resume", component: ResumeViewComponent },
-  { path: "projects", component: ProjectViewComponent },
+  { path: 'aboutMe', component: AboutMeViewComponent },
+  { path: 'resume', component: ResumeViewComponent },
+  { path: 'projects', component: ProjectViewComponent },
+  { path: '', redirectTo: '/aboutMe', pathMatch: 'full' },
 ];
 
 @NgModule({
