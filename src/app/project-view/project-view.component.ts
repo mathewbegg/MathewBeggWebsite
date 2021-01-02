52ede2695de72d24cbf85ebb99b48961b9f6f7af
Mathew <mathew.r.begg@gmail.com>
2021-01-01 17:16:58 -0700

    Added TKDDB to projects

--- src/app/project-view/project-view.component.ts
+++ src/app/project-view/project-view.component.ts
@@ -14,28 +14,39 @@ export class ProjectViewComponent implements OnInit {
   projects: ProjectData[] = [
     {
       name: "'Superfight' Social Tabletop Game",
-      address: 'http://superfight.mathewbegg.com',
+      address: 'https://superfight.mathewbegg.com',
       description: [
         "I created a browser adaptation of the social card game 'Superfight' so I could play it with friends virtually during the COVID-19 lockdown.",
-        'The client-side is an angular app hosted from an AWS S3 bucket, it connects via socket.io to a Node.js server hosted on an AWS EC2 instance.',
+        'The client-side is an angular web app hosted from an AWS S3 bucket, it connects via socket.io to a Node.js server hosted on an AWS EC2 instance.',
       ],
       iconURL: '../../assets/superfight-icon.png',
     },
     {
+      name: 'Martial Art School Student Database',
+      address: 'https://tkddb-demo.mathewbegg.com',
+      description: [
+        'This is an in-progress prototype of a cloud-based student database system designed for a martial arts school.',
+        'Student data is stored in JSON form, the application parses the data and displays/sorts each field according to configurable settings.',
+        'The client-side is an angular web app hosted from an AWS S3 bucket that reads/writes data from a severless AWS backend (API Gateway -> Lambda -> DynamoDB)',
+        'NOTE: For demo purposes, the app fetches randomly generated data from a mock data service.',
+      ],
+      iconURL: '../../assets/tkddb-icon.png',
+    },
+    {
       name: 'Tea Menu',
-      address: 'http://tea.mathewbegg.com',
+      address: 'https://tea.mathewbegg.com',
       description: [
         'The first app I created with a backend: a simple page showcasing a menu of teas.',
-        'It is an angular app hosted from an AWS S3 bucket that fetches data from a serverless AWS backend (API Gateway -> Lambda -> DynamoDB)',
+        'It is an angular web app hosted from an AWS S3 bucket that fetches data from a serverless AWS backend (API Gateway -> Lambda -> DynamoDB)',
       ],
       iconURL: '../../assets/tea-icon.png',
     },
     {
       name: "'Love Letter' Game Assistant",
-      address: 'http://loveletter.mathewbegg.com',
+      address: 'https://loveletter.mathewbegg.com',
       description: [
         'The first web app I created: a basic helper app for the tabletop game \'Love Letter\', in which one must keep track of "dead" and "alive" characters.',
-        'It is a simple angular app hosted from an AWS S3 bucket.',
+        'It is a simple angular web app hosted from an AWS S3 bucket.',
       ],
       iconURL: '../../assets/loveletter-icon.png',
     },
