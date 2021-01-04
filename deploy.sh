beb51769fd85ff4728803bcdd6ecfdc31941fe7b
Mathew <mathew.r.begg@gmail.com>
2021-01-04 14:41:50 -0700

    uncommented deploy code

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@ distributionID=E2JKNH26NX9G6F
 sourceDirectory=./dist/MathewBeggWebsite
 destinationDirectory=s3://mathewbegg.com
 
-# ECHO =================================
-# ECHO Intalling Dependencies
-# ECHO =================================
-# npm install
+ECHO =================================
+ECHO Intalling Dependencies
+ECHO =================================
+npm install
 
-# ECHO =================================
-# ECHO Building Project
-# ECHO =================================
-# ng build --prod
+ECHO =================================
+ECHO Building Project
+ECHO =================================
+ng build --prod
 
-# ECHO =================================
-# ECHO Syncing to s3
-# ECHO =================================
-# aws s3 sync $sourceDirectory $destinationDirectory --delete
+ECHO =================================
+ECHO Syncing to s3
+ECHO =================================
+aws s3 sync $sourceDirectory $destinationDirectory --delete
 
 ECHO =================================
 ECHO Invalidating Index.html in Cloudfront Distribution
