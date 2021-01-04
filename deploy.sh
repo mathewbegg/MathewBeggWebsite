12ab2c0b233abe9f79e3ab0e2078b90fa64e60a3
Mathew <mathew.r.begg@gmail.com>
2021-01-04 13:43:16 -0700

    Updated resume pdf, added assets folder to invalidation paths

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,22 @@ distributionID=E2JKNH26NX9G6F
 sourceDirectory=./dist/MathewBeggWebsite
 destinationDirectory=s3://mathewbegg.com
 
-ECHO =================================
-ECHO Intalling Dependencies
-ECHO =================================
-npm install
+# ECHO =================================
+# ECHO Intalling Dependencies
+# ECHO =================================
+# npm install
 
-ECHO =================================
-ECHO Building Project
-ECHO =================================
-ng build --prod
+# ECHO =================================
+# ECHO Building Project
+# ECHO =================================
+# ng build --prod
 
-ECHO =================================
-ECHO Syncing to s3
-ECHO =================================
-aws s3 sync $sourceDirectory $destinationDirectory --delete
+# ECHO =================================
+# ECHO Syncing to s3
+# ECHO =================================
+# aws s3 sync $sourceDirectory $destinationDirectory --delete
 
 ECHO =================================
 ECHO Invalidating Index.html in Cloudfront Distribution
 ECHO =================================
-aws cloudfront create-invalidation --distribution-id $distributionID --paths //index.html
+aws cloudfront create-invalidation --distribution-id $distributionID --paths  "//index.html" "/assets/*"
